4bac4f5a67f0953aebef878eca33408fbd781717
Marcelo Soares <marcelohsoares142@gmail.com>
2024-07-25 10:54:38 -0300

    1 page quase pronta

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,21 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
+const imagemin = require('gulp-imagemin');
 
 function styles() {
-    return gulp
-        .src("./src/styles/*.scss") // Retorna todos arquivos .scss de src
+    return gulp.src("./src/styles/*.scss") // Retorna todos arquivos .scss de src
         .pipe(sass({ outputStyle: "compressed" })) // passa os arquivos .scss para css e os "compacta" o maximo possivel, ganha-se mais performance
         .pipe(gulp.dest("./dist/css"));
 }
 
-exports.default = styles;
+function images() {
+    return gulp.src("./src/images/**/*") 
+        .pipe(imagemin())
+        .pipe(gulp.dest("./dist/images"));
+}
+
+exports.default = gulp.parallel(styles, images);
+
 exports.watch = function () {
     gulp.watch('./src/styles/*.scss', styles)
 }
